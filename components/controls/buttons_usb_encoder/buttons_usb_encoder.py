5204a715ac63a65b50f08f855065f5bdab3516b6
Julian Hundeloh <5358638+jaulz@users.noreply.github.com>
2025-06-07 20:32:06 +0200

    fix: check for correct type

--- components/controls/buttons_usb_encoder/buttons_usb_encoder.py
+++ components/controls/buttons_usb_encoder/buttons_usb_encoder.py
@@ -21,7 +21,7 @@ try:
             keyevent = categorize(event)
             if keyevent.keystate == KeyEvent.key_down:
                 button_string = keyevent.keycode
-                if type(button_string) is list:
+                if type(button_string) is list or type(button_string) is tuple:
                     button_string = '-'.join(sorted(button_string))
                 try:
                     function_name = button_map[button_string]
