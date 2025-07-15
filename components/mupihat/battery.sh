1cf0bb65fce59b0f37fec5dec36eeca6137acad2
Julian Hundeloh <julian@hundeloh-consulting.ch>
2025-07-15 14:25:38 +0000

    feat: use proper shutdown sound

--- components/mupihat/battery.sh
+++ components/mupihat/battery.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-SOUND_FILE="/home/pi/RPi-Jukebox-RFID/misc/battery-low.mp3"
+LOW_SOUND_FILE="/home/pi/RPi-Jukebox-RFID/misc/battery-low.mp3"
+SHUTDOWN_SOUND_FILE="/home/pi/RPi-Jukebox-RFID/shared/shutdownsound.mp3"
 JSON_FILE="/tmp/mupihat.json"
-
-play_sound() {
-    mpg123 -a hw:1,0 "$SOUND_FILE"
-}
 
 echo $! > /run/mupi_hat_control.pid
 sleep 30
@@ -19,11 +16,11 @@ if [ "${BAT_CONNECTED}" -eq 1 ]; then
             if [ "$VBUS" -le 1000 ]; then
 				STATE=$(jq -r '.Bat_Stat' ${JSON_FILE})
 				if [ "${STATE}" = "LOW" ]; then
-					play_sound
+					mpg123 -a hw:1,0 "$LOW_SOUND_FILE"
 					echo "Battery state low"
 				elif [ "${STATE}" = "SHUTDOWN" ]; then
 					echo "Battery state to low - shutdown initiated"
-					/usr/local/bin/mupibox/./mupi_shutdown.sh ${BATTERY_LOW}
+					mpg123 -a hw:1,0 "$SHUTDOWN_SOUND_FILE"
 					poweroff
 				fi
 			fi
